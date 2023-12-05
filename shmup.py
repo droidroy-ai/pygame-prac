a0cb67c33cf87805a51c0c02ca776619ef78c72e
droidroy-ai <roy.abhik.dev@gmail.com>
2023-12-06 04:03:50 +0530

    added shmup sounds and music

--- shmup.py
+++ shmup.py
@@ -1,3 +1,6 @@
+# Frozen Jam by tgfcoder <https://twitter.com/tgfcoder> licensed under CC-BY-3 <http://creativecommons.org/licenses/by/3.0/>
+# Art from Kenney.nl
+
 import pygame
 import random
 import os
@@ -23,6 +26,7 @@ clock = pygame.time.Clock()
 
 folder_dir = os.path.dirname(__file__)
 img_dir = os.path.join(folder_dir, 'img')
+snd_dir = os.path.join(folder_dir, 'snd')
 
 font_name = pygame.font.match_font('arial')
 def draw_text(surface, text: str, size: int, x, y):
@@ -63,6 +67,8 @@ class Player(pygame.sprite.Sprite):
         bullet = Bullet(self.rect.centerx, self.rect.top)
         all_sprites.add(bullet)
         bullets.add(bullet)
+        shoot_sound.play()
+
 
 class Mob(pygame.sprite.Sprite):
     def __init__(self):
@@ -119,7 +125,7 @@ class Bullet(pygame.sprite.Sprite):
             self.kill()
 
 
-# load all game graphics
+# Load all game graphics
 background = pygame.image.load(os.path.join(img_dir, 'starfield.png')).convert()
 background_rect = background.get_rect()
 player_img = pygame.image.load(os.path.join(img_dir, 'playerShip1_blue.png')).convert()
@@ -130,9 +136,17 @@ meteor_list = ['meteorGrey_big1.png', 'meteorGrey_big2.png',
                'meteorGrey_med1.png', 'meteorGrey_med2.png',
                'meteorGrey_small1.png', 'meteorGrey_small2.png',
                'meteorGrey_tiny1.png']
-
 for img in meteor_list:
     meteor_images.append(pygame.image.load(os.path.join(img_dir, img)).convert())
+
+# Load all the game sounds
+pygame.mixer.music.load(os.path.join(snd_dir, 'tgfcoder-FrozenJam-SeamlessLoop.ogg'))
+pygame.mixer.music.set_volume(0.4)
+
+shoot_sound = pygame.mixer.Sound(os.path.join(snd_dir, 'pew.wav'))
+explosion_sounds = []
+for snd in ['explosion_1.wav', 'explosion_2.wav']:
+    explosion_sounds.append(pygame.mixer.Sound(os.path.join(snd_dir, snd)))
 
 all_sprites = pygame.sprite.Group()
 mobs = pygame.sprite.Group()
@@ -147,6 +161,7 @@ for i in range(8):
 # scoring related stuff
 score = 0 
 
+pygame.mixer.music.play(loops=-1) # plays the background music
 # Game loop
 running = True
 while running:
@@ -167,6 +182,7 @@ while running:
     hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
     for hit in hits:
         score += 50 - hit.radius
+        random.choice(explosion_sounds).play()
         m = Mob()
         all_sprites.add(m)
         mobs.add(m)
