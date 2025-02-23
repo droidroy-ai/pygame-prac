3418ec235bae27efef7c8694d677ebfc5c472f6f
droidroy-ai <roy.abhik.dev@gmail.com>
2025-02-24 03:12:13 +0530

    created separate module for platfomer game

---
+++ platformer/main.py
@@ -0,0 +1,64 @@
+import pygame as pg
+import random
+
+from settings import *
+
+
+class Game:
+    def __init__(self):
+        # Intialize game window and other things
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption("Game")
+        self.clock = pg.time.Clock()
+        self.running = True
+
+    def new(self):
+        # resets the game and start a new game
+        self.all_sprites = pg.sprite.Group()
+        self.run()
+
+    def run(self):
+        # actual game loop
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+
+    def update(self):
+        # Game loop - update
+        self.all_sprites.update()
+
+    def events(self):
+        # Game loop - event
+        for event in pg.event.get():
+            # check for closing the window
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
+
+    def draw(self):
+        # Game loop - draw
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        # *after* drawing everything, flip the display
+        pg.display.flip()
+
+    def show_start_screen_(self):
+        pass
+
+    def show_go_screen(self):
+        pass
+
+
+game = Game()
+game.show_start_screen_()
+while game.running:
+    game.new()
+    game.show_go_screen()
+
+pg.quit()
